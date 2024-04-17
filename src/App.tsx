6d989179874bd0babd5a601c171e667739b39839
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-04-16 20:41:27 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from './components/Nav'; // Asegúrate de importar el componente Nav
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      {/* Otros componentes de tu portafolio irán aquí */}
+    </div>
+  );
+}
+
+export default App;
